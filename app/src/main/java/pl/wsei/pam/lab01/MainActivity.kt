f2247d3314c8fd1c66273ad7b528bc2de0f01278
Mati <mateusz.magiera010@gmail.com>
2024-03-12 11:25:41 +0100

    lob02

--- app/src/main/java/pl/wsei/pam/lab01/MainActivity.kt
+++ app/src/main/java/pl/wsei/pam/lab01/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toolbar.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
@@ -13,10 +14,22 @@ class MainActivity : AppCompatActivity() {
     lateinit var mTitle: TextView
     var mBoxes: MutableList<CheckBox> = mutableListOf()
     var mButtons: MutableList<Button> = mutableListOf()
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mLayout = findViewById(R.id.main)
+
+        var mProgress = ProgressBar(
+            this,
+            null,
+            androidx.appcompat.R.attr.progressBarStyle,
+            androidx.appcompat.R.style.Widget_AppCompat_ProgressBar_Horizontal
+        )
+        var progress = 0.0
 
         mTitle = TextView(this)
         mTitle.text = "Laboratorium 1"
@@ -29,67 +42,168 @@ class MainActivity : AppCompatActivity() {
 
         for (i in 1..6) {
             val checkBox = CheckBox(this)
+            val button = Button(this).also {
+                it.text="Testuj"
+                it.setOnClickListener {
+                    when (i) {
+                        1 -> {
+                                if (
+                                    task11(4, 6) in 0.666665..0.666667 &&
+                                    task11(7, -6) in -1.1666667..-1.1666665
+                                ) {
+                                    mBoxes[0].isChecked = true
+                                    progress+=17
+                                    mProgress.progress=progress.toInt()
+                                }
+
+                            }
+                        2-> {
+                                if (
+                                    task12(7U, 6U) == "7 + 6 = 13" &&
+                                    task12(12U, 15U) == "12 + 15 = 27"
+                                ) {
+                                    mBoxes[1].isChecked = true
+                                    progress+=17
+                                    mProgress.progress=progress.toInt()
+                                }
+                            }
+                        3-> {
+                                if (
+                                    task13(0.0, 5.4f) && !task13(7.0, 5.4f) &&
+                                    !task13(-6.0, -1.0f) && task13(6.0, 9.1f) &&
+                                    !task13(6.0, -1.0f) && task13(1.0, 1.1f)
+                                ) {
+                                    mBoxes[2].isChecked = true
+                                    progress+=17
+                                    mProgress.progress=progress.toInt()
+                                }
+                            }
+                        4-> {
+                                if (
+                                    task14(-2, 5) == "-2 + 5 = 3" &&
+                                    task14(-2, -5) == "-2 - 5 = -7"
+                                ) {
+                                    mBoxes[3].isChecked = true
+                                    progress+=17
+                                    mProgress.progress=progress.toInt()
+                                }
+                            }
+                        5-> {
+                                if (
+                                    task15("DOBRY") == 4 &&
+                                    task15("barDzo dobry") == 5 &&
+                                    task15("doStateczny") == 3 &&
+                                    task15("Dopuszczający") == 2 &&
+                                    task15("NIEDOSTATECZNY") == 1 &&
+                                    task15("XYZ") == -1
+                                ){
+                                    mBoxes[4].isChecked = true
+                                    progress+=17
+                                    mProgress.progress=progress.toInt()
+                                }
+                            }
+                        6-> {
+                                if (task16(
+                                        mapOf("A" to 2U, "B" to 4U, "C" to 3U),
+                                        mapOf("A" to 1U, "B" to 2U)
+                                    ) == 2U
+                                    &&
+                                    task16(
+                                        mapOf("A" to 2U, "B" to 4U, "C" to 3U),
+                                        mapOf("F" to 1U, "G" to 2U)
+                                    ) == 0U
+                                    &&
+                                    task16(
+                                        mapOf("A" to 23U, "B" to 47U, "C" to 30U),
+                                        mapOf("A" to 1U, "B" to 2U, "C" to 4U)
+                                    ) == 7U
+                                ) {
+                                    mBoxes[5].isChecked = true
+                                    progress+=17
+                                    mProgress.progress=progress.toInt()
+                                }
+                            }
+                    }
+
+                }
+            }
+            button.text="Testuj"
+            mButtons.add(button)
+
             checkBox.text = "Zadanie ${i}"
             checkBox.isEnabled = false
-            mLayout.addView(checkBox)
             mBoxes.add(checkBox)
+
+            val row = LinearLayout(this)
+            row.layoutParams = LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT
+            )
+            row.orientation = LinearLayout.HORIZONTAL
+
+            row.addView(checkBox)
+            row.addView(button)
+            mLayout.addView(row)
+
+
         }
 
-        if (
-            task11(4, 6) in 0.666665..0.666667 &&
-            task11(7, -6) in -1.1666667..-1.1666665
-        ) {
-            mBoxes[0].isChecked = true
-        }
+        mLayout.addView(mProgress)
 
+        /*
         if (
-            task12(7U, 6U) == "7 + 6 = 13" &&
-            task12(12U, 15U) == "12 + 15 = 27"
+        task11(4, 6) in 0.666665..0.666667 &&
+        task11(7, -6) in -1.1666667..-1.1666665
         ) {
-            mBoxes[1].isChecked = true
-        }
-
-        if (
-            task13(0.0, 5.4f) && !task13(7.0, 5.4f) &&
-            !task13(-6.0, -1.0f) && task13(6.0, 9.1f) &&
-            !task13(6.0, -1.0f) && task13(1.0, 1.1f)
-        ) {
-            mBoxes[2].isChecked = true
-        }
-
-        if (
-            task14(-2, 5) == "-2 + 5 = 3" &&
-            task14(-2, -5) == "-2 - 5 = -7"
-        ) {
-            mBoxes[3].isChecked = true
+        mBoxes[0].isChecked = true
         }
         if (
-            task15("DOBRY") == 4 &&
-            task15("barDzo dobry") == 5 &&
-            task15("doStateczny") == 3 &&
-            task15("Dopuszczający") == 2 &&
-            task15("NIEDOSTATECZNY") == 1 &&
-            task15("XYZ") == -1
+        task12(7U, 6U) == "7 + 6 = 13" &&
+        task12(12U, 15U) == "12 + 15 = 27"
+        ) {
+        mBoxes[1].isChecked = true
+        }
+        if (
+        task13(0.0, 5.4f) && !task13(7.0, 5.4f) &&
+        !task13(-6.0, -1.0f) && task13(6.0, 9.1f) &&
+        !task13(6.0, -1.0f) && task13(1.0, 1.1f)
+        ) {
+        mBoxes[2].isChecked = true
+        }
+        if (
+        task14(-2, 5) == "-2 + 5 = 3" &&
+        task14(-2, -5) == "-2 - 5 = -7"
+        ) {
+        mBoxes[3].isChecked = true
+        }
+        if (
+        task15("DOBRY") == 4 &&
+        task15("barDzo dobry") == 5 &&
+        task15("doStateczny") == 3 &&
+        task15("Dopuszczający") == 2 &&
+        task15("NIEDOSTATECZNY") == 1 &&
+        task15("XYZ") == -1
         ){
-            mBoxes[4].isChecked = true
+        mBoxes[4].isChecked = true
         }
         if (task16(
-                    mapOf("A" to 2U, "B" to 4U, "C" to 3U),
-                    mapOf("A" to 1U, "B" to 2U)
-                ) == 2U
-            &&
-            task16(
-                    mapOf("A" to 2U, "B" to 4U, "C" to 3U),
-                    mapOf("F" to 1U, "G" to 2U)
-                ) == 0U
-            &&
-            task16(
-                    mapOf("A" to 23U, "B" to 47U, "C" to 30U),
-                    mapOf("A" to 1U, "B" to 2U, "C" to 4U)
-                ) == 7U
-            ) {
-            mBoxes[5].isChecked = true
+        mapOf("A" to 2U, "B" to 4U, "C" to 3U),
+        mapOf("A" to 1U, "B" to 2U)
+        ) == 2U
+        &&
+        task16(
+        mapOf("A" to 2U, "B" to 4U, "C" to 3U),
+        mapOf("F" to 1U, "G" to 2U)
+        ) == 0U
+        &&
+        task16(
+        mapOf("A" to 23U, "B" to 47U, "C" to 30U),
+        mapOf("A" to 1U, "B" to 2U, "C" to 4U)
+        ) == 7U
+        ) {
+        mBoxes[5].isChecked = true
         }
+        */
     }
 
     // Wykonaj dzielenie niecałkowite parametru a przez b
@@ -98,10 +212,12 @@ class MainActivity : AppCompatActivity() {
         return a/b.toDouble()
     }
 
-    // Zdefiniuj funkcję, która zwraca łańcuch dla argumentów bez znaku (zawsze dodatnie) wg schematu
-    // <a> + <b> = <a + b>
-    // np. dla parametrów a = 2 i b = 3
-    // 2 + 3 = 5
+    /*
+Zdefiniuj funkcję, która zwraca łańcuch dla argumentów bez znaku (zawsze dodatnie) wg schematu
+<a> + <b> = <a + b>
+np. dla parametrów a = 2 i b = 3
+2 + 3 = 5
+*/
     private fun task12(a: UInt, b: UInt): String {
         return "$a + $b = ${a+b}"
     }
@@ -115,15 +231,17 @@ class MainActivity : AppCompatActivity() {
         return result
     }
 
-    // Zdefiniuj funkcję, która zwraca łańcuch dla argumentów całkowitych ze znakiem wg schematu
-    // <a> + <b> = <a + b>
-    // np. dla parametrów a = 2 i b = 3
-    // 2 + 3 = 5
-    // jeśli b jest ujemne należy zmienić znak '+' na '-'
-    // np. dla a = -2 i b = -5
-    //-2 - 5 = -7
-    // Wskazówki:
-    // Math.abs(a) - zwraca wartość bezwględną
+    /*
+Zdefiniuj funkcję, która zwraca łańcuch dla argumentów całkowitych ze znakiem wg schematu
+<a> + <b> = <a + b>
+np. dla parametrów a = 2 i b = 3
+2 + 3 = 5
+jeśli b jest ujemne należy zmienić znak '+' na '-'
+np. dla a = -2 i b = -5
+-2 - 5 = -7
+Wskazówki:
+Math.abs(a) - zwraca wartość bezwględną
+*/
     fun task14(a: Int, b: Int): String {
         if(b<0){
             return "$a - ${Math.abs(b)} = ${a+b}"
@@ -131,15 +249,17 @@ class MainActivity : AppCompatActivity() {
         return "$a + $b = ${a+b}"
     }
 
-    // Zdefiniuj funkcję zwracającą ocenę jako liczbę całkowitą na podstawie łańcucha z opisem słownym oceny.
-    // Możliwe przypadki:
-    // bardzo dobry 	5
-    // dobry 			4
-    // dostateczny 		3
-    // dopuszczający 	2
-    // niedostateczny	1
-    // Funkcja nie powinna być wrażliwa na wielkość znaków np. Dobry, DORBRY czy DoBrY to ta sama ocena
-    // Wystąpienie innego łańcucha w degree funkcja zwraca wartość -1
+    /*
+Zdefiniuj funkcję zwracającą ocenę jako liczbę całkowitą na podstawie łańcucha z opisem słownym oceny.
+Możliwe przypadki:
+bardzo dobry 	5
+dobry 			4
+dostateczny 		3
+dopuszczający 	2
+niedostateczny	1
+Funkcja nie powinna być wrażliwa na wielkość znaków np. Dobry, DORBRY czy DoBrY to ta sama ocena
+Wystąpienie innego łańcucha w degree funkcja zwraca wartość -1
+*/
     fun task15(degree: String): Int {
         val formattedDegree = degree.lowercase()
 
@@ -163,6 +283,21 @@ class MainActivity : AppCompatActivity() {
     // println(items)	=> 2 ponieważ do zbudowania jednego egzemplarza potrzebne są 2 elementy "B" i jeden "A", a w magazynie mamy 2 "A" i 4 "B",
     // czyli do zbudowania trzeciego egzemplarza zabraknie elementów typu "B"
     fun task16(store: Map<String, UInt>, asset: Map<String, UInt>): UInt {
-        return UInt.MAX_VALUE
+        var minCount = UInt.MAX_VALUE
+        var hasChanged = false
+
+        for (storeItem in store){
+            if(asset.containsKey(storeItem.key)){
+                var qty = storeItem.value/asset.getValue(storeItem.key)
+                if(qty<minCount){
+                    minCount=qty
+                    hasChanged=true
+                }
+            }
+
+
+        }
+
+        return if(hasChanged) minCount else 0u
     }
 }
